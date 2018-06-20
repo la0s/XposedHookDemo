a332676e7c371175528c1414e1e9d9ee3946b104
La0s <37521845+la0s@users.noreply.github.com>
2018-06-20 21:52:42 +0800

    Update XposedHook.java

--- app/src/main/java/com/example/la0s/xposedhook/XposedHook.java
+++ app/src/main/java/com/example/la0s/xposedhook/XposedHook.java
@@ -17,24 +17,24 @@ public class XposedHook implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
 
-        if (loadPackageParam.packageName.equals("cn.thecover.www.covermedia")) {
-            Class clasz = loadPackageParam.classLoader.loadClass("cn.thecover.www.covermedia.data.entity.HttpRequestEntity");
+        if (loadPackageParam.packageName.equals("cn.thecover.www.covermedia")) {    //过滤包名
+            Class clasz = loadPackageParam.classLoader.loadClass("cn.thecover.www.covermedia.data.entity.HttpRequestEntity"); //要hook的方法所在的类名
 
-            XposedHelpers.findAndHookMethod(clasz, "getSign",String.class,String.class,String.class, new XC_MethodHook() {
+            XposedHelpers.findAndHookMethod(clasz, "getSign",String.class,String.class,String.class, new XC_MethodHook() { //要hook的方法名和参数类型，此处为三个String类型
 
                 @Override
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
 
-                    Log.i("hook before param1:", (String) param.args[0]);
-                    Log.i("hook before param2:", (String) param.args[1]);
-                    Log.i("hook before param3:", (String) param.args[2]);
+                    Log.i("hook before param1:", (String) param.args[0]); //打印第一个参数
+                    Log.i("hook before param2:", (String) param.args[1]); //打印第一个参数
+                    Log.i("hook before param3:", (String) param.args[2]); //打印第一个参数
 
                 }
 
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
 
-                    Log.i("hook after result:",param.getResult().toString());
+                    Log.i("hook after result:",param.getResult().toString()); //打印返回值（String类型）
                 }
             });
         }
